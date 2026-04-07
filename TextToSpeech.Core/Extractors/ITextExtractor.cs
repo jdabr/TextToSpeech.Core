e2ac4bc2031b3fcd6db3267da0ea365879a87943
Jdabr <joeydabreu@gmail.com>
2026-04-07 11:17:02 -0400

    Text Extractor interface

--- TextToSpeech.Core/Extractors/ITextExtractor.cs
+++ TextToSpeech.Core/Extractors/ITextExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace TextToSpeech.Core.Extractors
 {
     internal class ITextExtractor
     {
+        public interface Itextextractor
+        {
+            bool CanHandle(string fileExtension);
+            Task<string> ExtractAsync(Stream fileStream);
+        }
     }
 }
