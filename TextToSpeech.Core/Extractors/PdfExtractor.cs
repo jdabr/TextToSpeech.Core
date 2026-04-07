3e973a839f0c08a2901aa9bc2bc6745f54125c94
Jdabr <joeydabreu@gmail.com>
2026-04-07 11:41:17 -0400

    Updated C# LangVersion to 8.0 & Added PdfPig extractor usage

--- TextToSpeech.Core/Extractors/PdfExtractor.cs
+++ TextToSpeech.Core/Extractors/PdfExtractor.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UglyToad.PdfPig;
 
 namespace TextToSpeech.Core.Extractors
 {
     internal class PdfExtractor
     {
+        public class pdfExtractor : ITextExtractor
+        {
+            public bool CanHandle(string ext) => ext == ".pdf";
+
+            public Task<string> ExtractAsync(Stream fileStream)
+            {
+                using var doc = PdfDocument.Open(fileStream);
+                var text = string.Join(" ", doc.GetPages()
+                    .SelectMany(p => p.GetWords())
+                    .Select(w => w.Text));
+                return Task.FromResult(text);
+            }
+        }
     }
 }
