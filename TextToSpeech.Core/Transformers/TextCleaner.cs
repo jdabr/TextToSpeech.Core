b4a36d00e70cd5f1190989c07ab836bd993c0b23
Jdabr <joeydabreu@gmail.com>
2026-04-08 15:04:28 -0400

    Basic Text Cleaner Code to remove whitspaces

--- TextToSpeech.Core/Transformers/TextCleaner.cs
+++ TextToSpeech.Core/Transformers/TextCleaner.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace TextToSpeech.Core.Transformers
+// TextCleaner.cs
+public class TextCleaner
 {
-    internal class TextCleaner
+    public string Clean(string raw)
     {
+        // Remove excess whitespace, fix smart quotes, etc.
+        var cleaned = Regex.Replace(raw, @"\s+", " ");
+        cleaned = cleaned.Replace("\u201C", "\"").Replace("\u201D", "\"");
+        return cleaned.Trim();
     }
 }
